a3e0bc359ebe040c124de7e0dc6131f0538039d8
Mike Falk <falk478@yahoo.com>
2021-01-18 18:17:36 -0800

    added mythical and regular animals

---
+++ FakeZoo/FakeZoo/Dragon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeZoo
+{
+    class Dragon : Mythical
+    {
+        public override bool Immortal { get ; set ; }
+        public override int Horns { get; set; }
+        public override string OriginRealm { get; set; }
+        public override int Eyes { get; set; }
+        public override int Legs { get; set; }
+
+       public Dragon()
+        {
+            Immortal = false;
+            Horns = 3;
+            OriginRealm = "Romania";
+            Eyes = 2;
+            Legs = 4;
+            Wings = true;
+            Diet = "Treasure";
+        }
+        public override string MagicPower()
+        {
+            return "soul meld";
+        }
+
+        public override string Speech()
+        {
+            return "Can speak ancient languages";
+        }
+
+        public override string Eat()
+        {
+            //Console.WriteLine("A dragon needs to eat a lot of treasure");
+            return "A dragon needs to eat a lot of treasure";
+        }
+    }
+}
